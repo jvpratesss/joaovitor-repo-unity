9f82ee99187bd720280e6344bd8b98bf8d00a690
Joao Vitor Prates <jv-prates@hotmail.com>
2020-06-30 16:17:25 -0300

    CollectibleGameObject, CointController

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : TouchableGameObject
     Camera cam;
     public LayerMask walkableLayer;
     public LayerMask collectibleLayer;
+    CollectibleGameObject pickupTarget;
 
     void Awake()
     {
@@ -19,6 +20,11 @@ public class PlayerController : TouchableGameObject
     }
 
     void Update()
+    {
+        ProcessInput();
+        UpdateCollect();
+    }
+    void ProcessInput()
     {
         if (Input.GetMouseButtonDown(1))
         {
@@ -33,9 +39,23 @@ public class PlayerController : TouchableGameObject
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, collectibleLayer))
             {
                 Debug.Log("Collectible: " + hit.collider.name);
+                pickupTarget = hit.collider.gameObject.GetComponent<CollectibleGameObject>();
                 agent.SetDestination(hit.point);
             }
-
+            else
+            {
+                pickupTarget = null;
+            }
+        }
+    }
+    void UpdateCollect()
+    {
+        if(pickupTarget != null)
+        {
+            if(IsInTouch(pickupTarget))
+            {
+                pickupTarget.Pickup();
+            }
         }
     }
 }
